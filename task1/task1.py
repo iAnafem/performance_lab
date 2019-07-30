87afd726e95b9acc3b154f12f4832564c3f91558
iAnafem <sidviciouc@gmail.com>
2019-07-30 23:44:44 +0300

    task1 complete

---
+++ task1/task1.py
@@ -0,0 +1,22 @@
+import sys
+import numpy
+
+
+def solution(test_file: str):
+    with open(test_file) as file:
+        array = [int(line) for line in file]
+        return (
+            numpy.percentile(array, 90),
+            numpy.median(array),
+            numpy.max(array),
+            numpy.min(array),
+            numpy.mean(array),
+        )
+
+
+if __name__ == '__main__':
+    for number in solution(sys.argv[1]):
+        print('{:0.2f}'.format(number))
+
+
+assert list(solution(sys.argv[1])) == [8.20, 5.00, 10.00, 0.00, 4.50]
